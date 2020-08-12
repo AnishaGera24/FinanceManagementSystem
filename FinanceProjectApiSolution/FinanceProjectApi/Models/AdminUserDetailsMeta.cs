5a3569c7b765a4d1271890f2601de0745b218335
arpitjain1009 <arpit.jain@lntinfotech.com>
2020-08-12 10:14:15 +0530

    Admin Meta class

---
+++ FinanceProjectApiSolution/FinanceProjectApi/Models/AdminUserDetailsMeta.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MyFinanceMgmtProject.Models
+{
+    [MetadataType(typeof(MetadataTypeForAdminUserDetails))]
+    public partial class AdminUserDetails
+    {
+    }
+    [DataContract]
+    public class MetadataTypeForAdminUserDetails
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string Firstname { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string UserName { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+        [DataMember]
+        public string PhoneNumber { get; set; }
+        [DataMember]
+        public string DOB { get; set; }
+        [DataMember]
+        public string DOJ { get; set; }
+        [DataMember]
+        public string EduQualification { get; set; }
+    }
+}
